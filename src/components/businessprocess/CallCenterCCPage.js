06218e1daeba5285d81209b83dd032e03f833b02
nucool <nop_za16@hotmail.com>
2019-02-06 16:18:42 +0700

    Modify Business Process

--- src/components/businessprocess/CallCenterCCPage.js
+++ src/components/businessprocess/CallCenterCCPage.js
@@ -20,15 +20,53 @@ const CallCenterCCPage = () => {
         <div className="col-sm-12">
           <h2>Call Center</h2>
           <div style={marginBox}>
-            <h4>Type-A (Insurance Only)</h4>
+            <h4>
+              <ul>
+                <li><b>Type-A (Insurance Only)</b></li>
+              </ul>
+            </h4>
             <div style={imageDiv}>
               <img alt="CallCenter Type-A" src={process.env.PUBLIC_URL + "/images/callcenterType-A.jpg"} />
             </div>
           </div>
           <div style={marginBox}>
-            <h4>Type-B (Insurance Only)</h4>
+            <h4>
+              <ul>
+                <li><b>Type-B (Insurance - Claim Di CallCenter & Insurance Surveyor)</b></li>
+              </ul>
+            </h4>
             <div style={imageDiv}>
               <img alt="CallCenter Type-B" src={process.env.PUBLIC_URL + "/images/callcenterType-B.jpg"} />
+            </div>
+          </div>
+          <div style={marginBox}>
+            <h4>
+              <ul>
+                <li><b>Type-C (Insurance - Claim Di CallCenter & Claim Di Surveyor)</b></li>
+              </ul>
+            </h4>
+            <div style={imageDiv}>
+              <img alt="CallCenter Type-C" src={process.env.PUBLIC_URL + "/images/callcenterType-C.jpg"} />
+            </div>
+          </div>
+          <div style={marginBox}>
+            <h4>
+              <ul>
+                <li><b>Type-D (Insurance - Claim Di CallCenter & Insurance Surveyor & Claim Di Surveyor)</b></li>
+              </ul>
+            </h4>
+            <div style={imageDiv}>
+              <img alt="CallCenter Type-D" src={process.env.PUBLIC_URL + "/images/callcenterType-D.jpg"} />
+            </div>
+          </div>
+          <div style={marginBox}>
+            <h4>
+              <ul>
+                <li><b>CallCenter Business Process Flow</b></li>
+              </ul>
+            </h4>
+            <div style={imageDiv}>
+              <img alt="CallCenter Business Process Flow" src={process.env.PUBLIC_URL + "/images/callcenterbusinessprocessflow.jpg"} />
             </div>
           </div>
         </div>
