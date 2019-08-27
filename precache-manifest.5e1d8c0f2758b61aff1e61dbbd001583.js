548f709a34fbba1c4296ca8ba031ba99fc6780af
nucool <nop_za16@hotmail.com>
2019-08-27 15:21:40 +0700

    Updates

---
+++ precache-manifest.5e1d8c0f2758b61aff1e61dbbd001583.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18a37259b6319596510cc49a6cb9c6cb",
+    "url": "/guideline/index.html"
+  },
+  {
+    "revision": "91355525118fa33eb0ec",
+    "url": "/guideline/static/css/2.67903297.chunk.css"
+  },
+  {
+    "revision": "b3ff3e4f172597d6d33e",
+    "url": "/guideline/static/css/main.542e4ca3.chunk.css"
+  },
+  {
+    "revision": "91355525118fa33eb0ec",
+    "url": "/guideline/static/js/2.76d3334a.chunk.js"
+  },
+  {
+    "revision": "b3ff3e4f172597d6d33e",
+    "url": "/guideline/static/js/main.0d5c0659.chunk.js"
+  },
+  {
+    "revision": "d7b4c27d73273373df90",
+    "url": "/guideline/static/js/runtime~main.500ca4b3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/guideline/precache-manifest.5167f2e31a708e4eb7e4e230a9e1b3a9.js"
+  "/guideline/precache-manifest.5e1d8c0f2758b61aff1e61dbbd001583.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/guideline/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/guideline/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
